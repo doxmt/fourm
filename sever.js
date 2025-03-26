08b8b26077dc1cbd3a9060bd749caa2c7810a9e7
Songtaeha <songtaeha@naver.com>
2025-03-26 21:33:37 +0900

    db연동

--- sever.js
+++ sever.js
@@ -3,6 +3,17 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://thdxogk2:ksh7803@cluster0.vdjyayk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+})
+
 app.listen(8080, () => { 
   console.log('http://localhost:8080 에서 서버 실행 중')
 })
@@ -12,7 +23,8 @@ app.get('/', (요청, 응답) => {
 })
 
 app.get('/news', (요청, 응답) => {
-  응답.send('오늘 비옴')
+  db.collection('post').insertOne({title : '어쩌구'})
+  // 응답.send('오늘 비옴')
 })
 
 app.get('/shop', (요청, 응답) => {
